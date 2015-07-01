d3bf77673baac2d8c70a518f6106f3486ca0fcb9
Jason Desiderio <jmdesiderio@gmail.com>
2015-06-30 20:47:02 -0400

    Initial commit

---
+++ assets/js/includes/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+    	closeOnClick: true,
+    	edge: 'right'
+    });
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
